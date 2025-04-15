4cd7baaea93291b45b5ba8ad0a18075113c6734b
Gakare <chiong_patrick@yahoo.com>
2025-04-15 01:23:00 +0000

    Adding suckless utilities

---
+++ suckless/dwm/colorschemes/dark_purple.h
@@ -0,0 +1,6 @@
+// clang-format off
+static const char col_dark_purple[]     = "#22222E";
+static const char col_dark_lavender[]   = "#393A5A";
+static const char col_lavender[]        = "#C8F4F9";
+static const char col_light_purple[]    = "#d1CEED";
+// clang-format on
